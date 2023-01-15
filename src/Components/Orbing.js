ffe4762c21279c1d490fc8792b594d748ff3256d
RJ Murray <therjmurray@gmail.com>
2023-01-15 16:32:32 -0500

    orb progress bar

--- src/Components/Orbing.js
+++ src/Components/Orbing.js
@@ -1,6 +1,25 @@
+import Orb1 from '../imgs/orb1.png'
+import Progress from './Progress'
+import { useState } from 'react'
+
 const Orbing = () => {
+    const [collecting, setCollecting] = useState('');
+
     return <div>
-        Orbing
+        <div onClick={collecting === 'Normal Orb' ? () => setCollecting('') : () => setCollecting('Normal Orb')} style={{width: 250, height: 250, background: '#505050', margin: '10px 0 0 10px'}}>
+            <div style={{height: 80, width: 80, margin: '0px auto'}}>
+                <img src={Orb1} alt="" style={{height: 80, width: 80, marginTop: 40}} />
+            </div>
+            <div style={{textAlign: 'center', marginTop: 40}}>
+                <div>Collect</div>
+                <div>Normal Orb</div>
+                <div>10 XP / 2 seconds</div>
+            </div>
+            <div style={{margin: '0px auto', width: 102}}>
+                <Progress collecting={collecting}/>
+            </div>
+            
+        </div>
     </div>
 }
 
--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,32 +1,28 @@
 import { useState, useEffect } from 'react'
-import Gary from '../imgs/gary.png'
 
-
-const Progress = () => {
+const Progress = ({collecting}) => {
     const [progress, setProgress] = useState(0);
-    const [gary, setGary] = useState(0);
     
     useEffect(() => {
 		let interval = null;
-		if (progress < 100) {
-			interval = setInterval(() => {
-				setProgress(prevProgress => prevProgress + 1);
-			}, 50);
-		} else {
-			setProgress(0);
-			setGary(gary + 1);
-		}
-		return () => clearInterval(interval);
-	}, [progress, gary]);
+        if (collecting.length > 0){
+            if (progress < 100) {
+                interval = setInterval(() => {
+                    setProgress(prevProgress => prevProgress + 1);
+                }, 50);
+            } else {
+                setProgress(0);
+            }
+            return () => clearInterval(interval);
+        }
+	}, [progress, collecting]);
     
     return <div>
-        <img src={Gary} alt="" style={{width: 40, height: 40}} />
         <div style={{height: 40, width: 100, border: '1px solid black'}}>
             <div style={{height: 40, width: `${progress}%`, background: 'green'}}>
 
             </div>
         </div>
-        <div>Garys: {Gary}</div>
     </div>
 }
 
